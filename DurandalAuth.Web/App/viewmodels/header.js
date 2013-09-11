0b908b8dc5eba537e5794bf96725d04e30e6be0f
Yago Pérez Vázquez <yagoperez@outlook.com>
2013-09-12 00:49:03 +0200

    Creating header.js

---
+++ DurandalAuth.Web/App/viewmodels/header.js
@@ -0,0 +1,30 @@
+﻿define(['plugins/router', 'services/appsecurity', 'services/errorhandler'],
+    function (router, appsecurity, errorhandler) {        
+
+        var viewmodel = {
+
+            router: router,
+
+            appsecurity: appsecurity,
+
+            logout: function () {
+                var self = this;
+                appsecurity.logout().fail(self.handlevalidationerrors);
+            },
+
+            activate: function () {
+                var self = this;
+
+                return appsecurity.getAuthInfo()
+                        .then(function (authinfo) {
+                            // Get current auth info
+                            self.appsecurity.user(authinfo);
+                        })
+                        .fail(self.handlevalidationerrors);
+            }
+        };
+
+        errorhandler.includeIn(viewmodel);
+
+        return viewmodel;
+    });
