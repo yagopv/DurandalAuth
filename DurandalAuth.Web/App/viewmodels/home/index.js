4096aea2c855917fb246a8c0170728e49c147f96
Yago Pérez Vázquez <yagoperez@outlook.com>
2013-09-16 19:38:15 +0200

    Fix url links index.html for working with pushState

--- DurandalAuth.Web/App/viewmodels/home/index.js
+++ DurandalAuth.Web/App/viewmodels/home/index.js
@@ -1,6 +1,8 @@
-﻿define(function () {
+﻿define(['plugins/router'], function (router) {
 
     return {
+
+        convertRouteToHash: router.convertRouteToHash,
 
         activate: function () {
             ga('send', 'pageview', { 'page': window.location.href, 'title': document.title });
