2792a1ec82b0b8e174f385eded33d6fa6b82a2d1
Yago Pérez Vázquez <yagoperez@outlook.com>
2013-10-05 02:34:49 +0200

    using routes instead url rewrite

--- DurandalAuth.Web/App_Start/RouteConfig.cs
+++ DurandalAuth.Web/App_Start/RouteConfig.cs
@@ -46,8 +46,8 @@ namespace DurandalAuth.Web
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{*url}",
+                defaults: new { controller = "Home", action = "Index" }
             );
         }
     }
