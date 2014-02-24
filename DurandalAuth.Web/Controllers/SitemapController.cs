a6ea4ef04f8e31ffecc2e5f9c381c96ef459c44a
jmwnoble <jmwnoble@gmail.com>
2014-02-24 18:01:42 +1000

    Changed sitemap controller and robots.txt

--- DurandalAuth.Web/Controllers/SitemapController.cs
+++ DurandalAuth.Web/Controllers/SitemapController.cs
@@ -11,41 +11,42 @@ using DurandalAuth.Domain.UnitOfWork;
 
 namespace DurandalAuth.Web.Controllers
 {
-    public class SitemapController : Controller
-    {
-        private IUnitOfWork UnitOfWork;
+	public class SitemapController : Controller
+	{
+		private IUnitOfWork UnitOfWork;
 
-        public SitemapController(IUnitOfWork unitofwork)
-        {
-            this.UnitOfWork = unitofwork;
-        }
+		public SitemapController(IUnitOfWork unitofwork)
+		{
+			this.UnitOfWork = unitofwork;
+		}
 
-        //
-        // GET: /Sitemap/
-        public ActionResult Sitemap()
-        {
-            var url = System.Web.HttpContext.Current.Request.Url;
-            var leftpart = url.Scheme + "://" + url.Authority;
+		//
+		// GET: /Sitemap/
+		public ActionResult Sitemap()
+		{
+			var url = System.Web.HttpContext.Current.Request.Url;
+			var leftpart = url.Scheme + "://" + url.Authority;
 
-            // Static Urls
-            var sitemapItems = new List<SitemapItem>
-            {                
-                new SitemapItem(leftpart + "/home/index", changeFrequency: SitemapChangeFrequency.Weekly, priority: 1.0),
-                new SitemapItem(leftpart + "/home/articles", changeFrequency: SitemapChangeFrequency.Daily, priority: 1.0),
-                new SitemapItem(leftpart + "/home/help", changeFrequency: SitemapChangeFrequency.Weekly, priority: 1.0),
-                new SitemapItem(leftpart + "/home/about", changeFrequency: SitemapChangeFrequency.Monthly, priority: 1.0),
-                new SitemapItem(leftpart + "/account/login", changeFrequency: SitemapChangeFrequency.Monthly, priority: 0.5),
-                new SitemapItem(leftpart + "/account/register", changeFrequency: SitemapChangeFrequency.Monthly, priority: 0.5)
-            };
+			// Static Urls
+			var sitemapItems = new List<SitemapItem>
+			{                
+				new SitemapItem(leftpart + "/home/index", changeFrequency: SitemapChangeFrequency.Weekly, priority: 1.0),
+				new SitemapItem(leftpart + "/home/articles", changeFrequency: SitemapChangeFrequency.Daily, priority: 1.0),
+				new SitemapItem(leftpart + "/home/contact", changeFrequency: SitemapChangeFrequency.Daily, priority: 1.0),
+				new SitemapItem(leftpart + "/home/help", changeFrequency: SitemapChangeFrequency.Weekly, priority: 1.0),
+				new SitemapItem(leftpart + "/home/about", changeFrequency: SitemapChangeFrequency.Monthly, priority: 1.0),
+				new SitemapItem(leftpart + "/account/login", changeFrequency: SitemapChangeFrequency.Monthly, priority: 0.5),
+				new SitemapItem(leftpart + "/account/register", changeFrequency: SitemapChangeFrequency.Monthly, priority: 0.5)
+			};
 
-            var articles = UnitOfWork.ArticleRepository.All().Where(a => a.IsPublished);
+			var articles = UnitOfWork.ArticleRepository.All().Where(a => a.IsPublished);
 
-            foreach (var article in articles)
-            {
-                sitemapItems.Add(new SitemapItem(leftpart + "/" + article.CreatedBy + "/" + article.Category.Name + "/" + article.UrlCodeReference, changeFrequency: SitemapChangeFrequency.Weekly, priority: 0.8));
-            }
+			foreach (var article in articles)
+			{
+				sitemapItems.Add(new SitemapItem(leftpart + "/" + article.CreatedBy + "/" + article.Category.Name + "/" + article.UrlCodeReference, changeFrequency: SitemapChangeFrequency.Weekly, priority: 0.8));
+			}
 
-            return new SitemapResult(sitemapItems);
-        }
+			return new SitemapResult(sitemapItems);
+		}
 	}
 }
